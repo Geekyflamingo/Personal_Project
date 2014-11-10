304f96fca3ef4573fbf22c793952b75c110ebe21
Becca <rebecca.vasil@gmail.com>
2014-11-09 21:24:53 -0700

    sign in and sign up basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   root "pages#index"
 
+  resources :users
 
+get '/sign-up' => 'registrations#new', as: :signup
+post '/sign-up' => 'registrations#create'
+get '/sign-in' => 'authentication#new', as: :signin
+post '/sign-in' => 'authentication#create'
+get '/sign-out' => 'authentication#destroy', as: :signout
 
 
   # The priority is based upon order of creation: first created -> highest priority.

84728429da74d51afff7da54775b40f82d828abc
Becca <rebecca.vasil@gmail.com>
2015-01-09 14:59:02 -0700

    moved js functions out of script tag in application file  into js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,107 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var stageWidth = 1000;
+var stageHeight = 800;
+var stage;
+var update = true;
+var shape2;
+var rotate_clockwise;
+var img;
+var rotdegr = 3;
+var timer;
+
+function init(){
+
+  //new stage
+  stage = new createjs.Stage(document.getElementById("myCanvas"));
+  stage.enableMouseOver();
+
+  //rectangle
+  shape2 = new createjs.Shape();
+  shape2.graphics.beginFill(createjs.Graphics.getRGB(255,0,0));
+  shape2.graphics.rect(0,0,100,10);
+  shape2.regX = 50;
+  shape2.regY = 5;
+  shape2.x = stageWidth/2;
+  shape2.y = stageHeight/2;
+  stage.autoClear = true;
+  stage.enableDOMEvents(true);
+  createjs.Touch.enable(stage);
+  createjs.Ticker.setFPS(30);
+  shape2.snapToPixel = true;
+  shape2.mouseEnabled = true;
+  shape2.alpha = 0.7;
+  // shape2.on("dblclick", handleMouseDown);
+
+  stage.update();
+  // using "on" binds the listener to the scope of the currentTarget by default
+  // in this case that means it executes in the scope of the button.
+  shape2.on("mousedown", function (evt) {
+    this.parent.addChild(this);
+    console.log(this, evt);
+    this.offset = {
+      x: this.x - evt.stageX,
+      y: this.y - evt.stageY
+    };
+  });
+  // the pressmove event is dispatched when the mouse moves after a mousedown on the target until the mouse is released.
+  shape2.on("pressmove", function (evt) {
+    console.log(this.offset);
+    console.log(Math.sin(shape2.rotation));
+    // if(Math.abs(Math.sin(shape2.rotation)) < .15)){
+    //
+    // }else{
+    //
+    // }
+    if( Math.abs(this.offset.x) > 20 ){
+      rotateCounter();
+    }else{
+      this.x = evt.stageX + this.offset.x;
+      this.y = evt.stageY + this.offset.y;
+      // indicate that the stage should be updated on the next tick:
+      update = true;
+    }
+  });
+  stage.addChild(shape2);
+
+  //draw to the canvas
+  createjs.Ticker.addEventListener("tick",tick);
+}
+
+
+function pressHandler(e){
+  e.onMouseMove = function(ev){
+    e.target.x = ev.stageX;
+    e.target.y = ev.stageY;
+    update = true;
+  }
+}
+
+function tick(event) {
+  // this set makes it so the stage only re-renders when an event handler indicates a change has happened.
+  if (update) {
+    update = false; // only update once
+    stage.update(event);
+  }
+}
+
+function stop() {
+  createjs.Ticker.removeEventListener("tick", tick);
+}
+
+function rotateCounter() {
+  shape2.rotation += -3;
+  update = true;
+}
+
+// function rotateWheel(type){
+// if(type){
+//   //clockwise
+//   shape2.rotation = shape2.rotation + rotdegr;
+// }else{
+//   //anti clockwise
+//   shape2.rotation = shape2.rotation - rotdegr;
+// }
+// stage.update();
+// }

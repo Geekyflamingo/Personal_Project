918ab780b99153b1f7d2cd32eabe004b6bdf63df
Becca <rebecca.vasil@gmail.com>
2015-02-18 22:43:11 -0700

    different types of jumps can be added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,16 +61,123 @@ function init(){
   article = document.getElementById('user');
 }
 
-function newJump(){
+function single(){
   var l1 = new fabric.Rect({left: 100, top: 6, width:80, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
-  var l2 = new fabric.Rect({left: 100, width:80, height: 21, fill: 'blue'});
   var end1 = new fabric.Rect({left:100, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
   var end2 = new fabric.Rect({left:180, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
   var str1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
   var str2 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
   var str3 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
-  var num = new fabric.Text("",{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
   var jump = new fabric.Group([ l1, end1, end2,str1, str2, str3, num],{left:500, top:500}); // This is the problem!
+  return jump;
+}
+
+function double(){
+  var end1 = new fabric.Rect({left:100, width: 10, height: 30, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  var end2 = new fabric.Rect({left:180, width: 10, height: 30, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+
+  var p1 = new fabric.Rect({left: 110, top: 6, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p1 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p1 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var p2 = new fabric.Rect({left: 110, top: 18, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p2 = new fabric.Rect({left:120, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p2 = new fabric.Rect({left:140, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p2 = new fabric.Rect({left:160, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  var jump = new fabric.Group([end1, end2, p1, str1p1, str2p1, str3p1,  p2, str1p2, str2p2, str3p2, num],{left:500, top:500}); // This is the problem!
+  return jump;
+}
+
+function triple(){
+  var end1 = new fabric.Rect({left:100, width: 10, height: 42, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  var end2 = new fabric.Rect({left:180, width: 10, height: 42, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+
+  var p1 = new fabric.Rect({left: 110, top: 6, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p1 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p1 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var p2 = new fabric.Rect({left: 110, top: 18, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p2 = new fabric.Rect({left:120, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p2 = new fabric.Rect({left:140, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p2 = new fabric.Rect({left:160, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var p3 = new fabric.Rect({left: 110, top: 30, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p3 = new fabric.Rect({left:120, top: 30,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p3 = new fabric.Rect({left:140, top: 30,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p3 = new fabric.Rect({left:160, top: 30,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  var jump = new fabric.Group([end1, end2, p1, str1p1, str2p1, str3p1,  p2, str1p2, str2p2, str3p2,   p3, str1p3, str2p3, str3p3, num],{left:500, top:500}); // This is the problem!
+  return jump;
+}
+
+function singliverpool(){
+  var l2 = new fabric.Rect({left: 114, top: -2, width:64, height: 22, fill: 'blue'});
+  var l1 = new fabric.Rect({left: 100, top: 6, width:80, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var end1 = new fabric.Rect({left:100, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  var end2 = new fabric.Rect({left:180, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  var str1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+  var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  var jump = new fabric.Group([ l2, l1, end1, end2,str1, str2, str3, num],{left:500, top:500}); // This is the problem!
+  return jump;
+}
+
+function doubliverpool(){
+  var liv =  new fabric.Rect({left: 113, top: -3, width:64, height: 36, fill: 'blue'});
+  var end1 = new fabric.Rect({left:100, width: 10, height: 30, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  var end2 = new fabric.Rect({left:180, width: 10, height: 30, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+
+  var p1 = new fabric.Rect({left: 110, top: 6, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p1 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p1 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var p2 = new fabric.Rect({left: 110, top: 18, width:70, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  var str1p2 = new fabric.Rect({left:120, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  var str2p2 = new fabric.Rect({left:140, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  var str3p2 = new fabric.Rect({left:160, top: 18,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+
+  var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  var jump = new fabric.Group([ liv, end1, end2, p1, str1p1, str2p1, str3p1,  p2, str1p2, str2p2, str3p2, num],{left:500, top:500}); // This is the problem!
+  return jump;
+}
+
+function newJump(){
+  var jump;
+  // var l1 = new fabric.Rect({left: 100, top: 6, width:80, height: 6, fill: document.getElementById('colorPick2').style.cssText.split(":")[1]});
+  // var l2 = new fabric.Rect({left: 100, width:80, height: 21, fill: 'blue'});
+  // var end1 = new fabric.Rect({left:100, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  // var end2 = new fabric.Rect({left:180, width: 11, height: 18, fill: document.getElementById('colorPick1').style.cssText.split(":")[1]});
+  // var str1 = new fabric.Rect({left:120, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick3').style.cssText.split(":")[1]});
+  // var str2 = new fabric.Rect({left:140, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick4').style.cssText.split(":")[1]});
+  // var str3 = new fabric.Rect({left:160, top: 6,  width: 10, height: 6, fill: document.getElementById('colorPick5').style.cssText.split(":")[1]});
+  // var num = new fabric.Text(document.getElementById('ordering').value,{ fontSize: 15, originX:'center', originY:'center', left: 90, top: 10,});
+  // var jump = new fabric.Group([ l1, end1, end2,str1, str2, str3, num],{left:500, top:500}); // This is the problem!
+  sing = document.getElementById('jumpType1').checked
+  ox = document.getElementById('jumpType2').checked
+  trip = document.getElementById('jumpType3').checked
+  singliv = document.getElementById('jumpType4').checked
+  oxliv = document.getElementById('jumpType5').checked
+
+  if(sing){
+    jump = single();
+  }else if(singliv){
+      jump = singliverpool();
+  }else if(ox){
+    jump = double();
+  }else if(oxliv){
+      jump = doubliverpool();
+  }else if(trip){
+    jump = triple();
+  }
+
   jump.set('selectable', true);
   jump.lockScalingX = true;
   jump.lockScalingY = true;
@@ -81,9 +188,6 @@ function newJump(){
     cornerSize: 8,
     transparentCorners: true,
   });
-  // l1.on('event:selected', function (options) {
-  //   console.log('mouse:dblclick');
-  // });
   stage.add(jump);
 }
 
